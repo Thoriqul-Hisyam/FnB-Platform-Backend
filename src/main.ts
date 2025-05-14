98787c3d61f9c570fcf3909d1c3b9edecd0abbb9
thoriqul-hisyam <thoriqulh12@gmail.com>
2025-05-14 16:37:21 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: true,
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'https://fnb-platform-backend.vercel.app',
+      credentials: true,
+    },
   });
-
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
