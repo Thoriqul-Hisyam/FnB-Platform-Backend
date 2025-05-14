6a08b7d2ae61329fc863f33138864da99503967a
thoriqul-hisyam <thoriqulh12@gmail.com>
2025-05-14 16:51:05 +0800

    update cors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: 'http://localhost:3000', // alamat frontend Next.js
-      credentials: true, // jika pakai cookies atau auth header
-    },
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
